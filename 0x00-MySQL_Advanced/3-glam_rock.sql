4facfce9888badf3bdb87527b2d8d1330d6848fe
colly-m <sindanicolly@gmail.com>
2024-05-09 21:30:52 +0000

    Old school B

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- Script to list all bands with Glam rock as main style
+-- Ranked by longevity
+
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
