ef43a603e89fe37dac67d8fbc9687bb2e103eb9e
colly-m <sindanicolly@gmail.com>
2024-05-09 20:57:14 +0000

    In n not Out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Sceipt to create a table users with country ENUM
+
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id),
+	county ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
