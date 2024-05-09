f0b7fda8d1a0c413984c898d7621effca2faaf90
colly-m <sindanicolly@gmail.com>
2024-05-09 20:38:22 +0000

    All unique fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- Script to create a table users
 
 CREATE TABLE IF NOT EXISTS users (
-	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id int NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
+	PRIMARY KEY (id)
 );
