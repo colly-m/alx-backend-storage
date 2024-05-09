864f87fc973605a4307e5620380376da7fd6fb5c
colly-m <sindanicolly@gmail.com>
2024-05-09 22:13:56 +0000

    ADDing bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,18 @@
+-- Script to create a stored procedure bonus to add new correction for student
+
+DELIMETER //
+DROP PROCEDURE IF EXISTS AddBonus;
+CREATE PROCEDURE AddBonus(
+	IN `iser_id` INTEGER,
+	IN `project_name` VARCHAR(255),
+	In `score` INTEGER
+)
+BEGIN
+	INSERT INTO projects (name)
+	SELECT project_name
+	WHERE project_name NOT IN (SELECTED name FROM projects);
+
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
+END //
+DELIMETER ;//
