e148fe47712ab1ecf4438d9628eaa085457129b5
colly-m <sindanicolly@gmail.com>
2024-05-09 20:58:46 +0000

    In n out fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS users (
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
 	PRIMARY KEY (id),
-	county ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
