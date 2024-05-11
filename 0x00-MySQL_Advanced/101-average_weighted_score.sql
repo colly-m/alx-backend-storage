d6bb40521bc03a4d2722f51b20681c68f8018d4b
colly-m <sindanicolly@gmail.com>
2024-05-11 22:39:50 +0000

    Avg score for all

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -10,6 +10,6 @@ BEGIN
 	FROM corrections
 	INNER JOIN projects
 	ON projects.id = corrections.project_id
-	WHERE corrections.user_id = users.id);
+	where corrections.user_id = users.id);
 END //
-DELIMITER ;
+DELIMITER ;//
