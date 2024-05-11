464e608b049540efdc1c65fffb14303e4ab97450
colly-m <sindanicolly@gmail.com>
2024-05-11 21:27:19 +0000

    Opt simple fix

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- Script to create an index on the table names
 -- and first letter of name and the score
 
-CREATE TABLE idx_name_first_score ON names(name(1), score);
+CREATE INDEX idx_name_first_score ON names(name(1), score);
