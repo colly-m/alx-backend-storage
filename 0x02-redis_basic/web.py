e95cbabd7180f8401bcdd104051e461e28d5f46e
colly-m <sindanicolly@gmail.com>
2024-05-15 23:40:15 +0000

    Expiration web update fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,15 +7,27 @@ import redis
 
 redis_client = redis.StrictRedis()
 
+
+def cache_decorator(func):
+    def wrapper(url):
+        """Defines a method to check if URL is in cache and not expired"""
+        if url in cache and time.time() - cache[url]["timestamp"] < 10:
+            cache[url]["count"] += 1
+            return cache[url]["content"]
+
+        content = func(url)
+
+        cache[url] = {
+            "content": content,
+            "timestamp": time.time(),
+            "count": 1
+        }
+        
+        return content
+    return wrapper
+
+
 def get_page(url: str) -> str:
-    """Defines a method to get a self descriptive page and returns the HTML contents"""
-    cached_content = redis_client.get(url)
-    if cached_content:
-        return cached_content.decode('utf-8')
-
+    """Function to get HTML content of a particular URL"""
     response = requests.get(url)
-    content = response.text
-
-    redis_client.setex(url, 10, content)
-
-    return content
+    return response.text
