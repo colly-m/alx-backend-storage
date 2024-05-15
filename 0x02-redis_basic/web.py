9d2a9c15952885019af83c1d04f029a9832e1277
colly-m <sindanicolly@gmail.com>
2024-05-15 23:48:16 +0000

    Expiration web update fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,31 +2,32 @@
 """Function to implement a get_page using requests"""
 import requests
 import time
+from typing import Callable
 import redis
+from functools import wraps
 
 
-redis_client = redis.StrictRedis()
+redis = redis.Redis()
 
 
-def cache_decorator(func):
+def wrap_decorator(func: Callable) -> Callable:
+    """Defines a decorator wrapper"""
+
+    @wraps(func)
     def wrapper(url):
         """Defines a method to check if URL is in cache and not expired"""
-        if url in cache and time.time() - cache[url]["timestamp"] < 10:
-            cache[url]["count"] += 1
-            return cache[url]["content"]
-
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
         content = func(url)
-
-        cache[url] = {
-            "content": content,
-            "timestamp": time.time(),
-            "count": 1
-        }
-        
+        redis.setex(f"cached:{url}", 10, content) 
         return content
+
     return wrapper
 
 
+@wrap_decorators
 def get_page(url: str) -> str:
     """Function to get HTML content of a particular URL"""
     response = requests.get(url)
